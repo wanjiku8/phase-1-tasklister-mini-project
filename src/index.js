01bb6841aa7b2b91afe29c47a04106bca35bc976
Faith Wanjiku <wanjikuf141@gmail.com>
2024-12-08 03:06:08 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.getElementById("create-task-form");
+  const taskList = document.getElementById("tasks");
+
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const taskInput = document.getElementById("new-task-description");
+    const taskDescription = taskInput.value;
+
+    const taskItem = document.createElement("li");
+    taskItem.textContent = taskDescription;
+
+    taskList.appendChild(taskItem);
+
+    taskInput.value = "";
+  });
 });
